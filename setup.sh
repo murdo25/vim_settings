01af06e8fa41a640f4be2ce16909dfc70b0587c0
EC2 Default User <ec2-user@ip-172-31-10-178.ec2.internal>
2019-09-24 01:00:17 +0000

    added directory checking

--- setup.sh
+++ setup.sh
@@ -6,18 +6,37 @@ echo "ARE YOU SURE YOU WANT TO DELETE YOUR .vimrc? ([Y/N] followed by [ENTER])"
 read choice
 
 if ( "$choice" == "Y" );  then
-    rm ~/.vimrc
+    rm ~/.vimrc;
 fi
 
 echo "pathogen setup"
-mkdir -p ~/.vim/autoload ~/.vim/bundle # && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo "adding python-mode"
-git clone --recurse-submodules https://github.com/python-mode/python-mode.git ~/.vim/bundle
+if [ ! -d ~/.vim/autoload ]; then
+  echo "made autoload"
+  mkdir -p  ~/.vim/autoload
+fi
 
-echo "adding vim sensable"
-git clone https://tpope.io/vim/sensible.git ~/.vim/bundle
+if [ ! -d ~/.vim/bundle ]; then
+  echo "made bundle"
+  mkdir -p  ~/.vim/bundle
+fi
+
+
+
+if [ ! -d ~/.vim/autoload/pathogen.vim ]; then
+  echo "pathogen downloading"
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if [ ! -d ~/.vim/bundle/python-mode ]; then
+  echo "adding python-mode"
+  git clone --recurse-submodules https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode
+fi
+
+if [ ! -d ~/.vim/bundle/vim-sensible ]; then
+  echo "adding vim sensable"
+  git clone https://tpope.io/vim/sensible.git ~/.vim/bundle/vim-sensible
+fi
 
 
 echo "make sure the good .vimrc is in this folder"
